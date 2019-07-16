0a25a7965ece59b5282fe286f9424da145ac7942
Alan Chu <alan_chu@yahoo.com>
2019-07-16 16:30:22 +1000

    Fixed indentation to 2 spaces

--- splat.js
+++ splat.js
@@ -11,30 +11,30 @@ hit.src = "sounds/Space-Cannon.mp3";
 
 // Cannon
 const cannon = {
-    x : 0, // left side of canvas
-    y : (canvas.height - 100)/2, // -100 the cannon.height of paddle
-    width : 100,
-    height : 20,
-    color : "BLUE",
-    splatOn : false,
-	splatX : 0,
-	splatY : 0,
-	splatAngle : 0,
-	splatTravel : 0,
-	oldAngle : 0,
-	xPos : 50,
-	yPos : 250,
-	angle : 120
-	
+  x : 0, // left side of canvas
+  y : (canvas.height - 100)/2, // -100 the cannon.height of paddle
+  width : 100,
+  height : 20,
+  color : "BLUE",
+  splatOn : false,
+  splatX : 0,
+  splatY : 0,
+  splatAngle : 0,
+  splatTravel : 0,
+  oldAngle : 0,
+  xPos : 50,
+  yPos : 250,
+  angle : 120
+
 }
 
 // draw circle, will be used to draw the ball
 function drawArc(x, y, r, color){
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.arc(x,y,r,0,Math.PI*2,true);
-    ctx.closePath();
-    ctx.fill();
+	ctx.fillStyle = color;
+	ctx.beginPath();
+	ctx.arc(x,y,r,0,Math.PI*2,true);
+	ctx.closePath();
+	ctx.fill();
 }
 
 function angle2radians(angle){
@@ -45,9 +45,9 @@ canvas.addEventListener('mousemove', getMousePos);
 canvas.addEventListener('click', splat);
 function splat(evt) {
 	var x,y,radians, steps;
-    if (cannon.splatOn == true)
+	if (cannon.splatOn == true)
 	{ 
-	    return;
+		return;
 	}
 	
 	radians = angle2radians(cannon.angle);
@@ -55,16 +55,16 @@ function splat(evt) {
 	x = cannon.xPos+cannon.width/2;
 	y = cannon.yPos+cannon.height/2;
 	hit.play();
-    cannon.splatOn = true;
+	cannon.splatOn = true;
 	cannon.splatX = x;
 	cannon.splatY = y;
 	cannon.splatAngle = radians;
 	cannon.splatTravel = 0.5;
 }	
 function getMousePos(evt){
-    let rect = canvas.getBoundingClientRect();
-    
-    y = evt.clientY - rect.top - cannon.height/2;
+	let rect = canvas.getBoundingClientRect();
+
+	y = evt.clientY - rect.top - cannon.height/2;
 	cannon.angle = y - 50;
 }
 
@@ -72,9 +72,9 @@ function getMousePos(evt){
 
 // draw text
 function drawText(text,x,y){
-    ctx.fillStyle = "#FFF";
-    ctx.font = "75px fantasy";
-    ctx.fillText(text, x, y);
+	ctx.fillStyle = "#FFF";
+	ctx.font = "75px fantasy";
+	ctx.fillText(text, x, y);
 }
 
 
@@ -96,37 +96,37 @@ function drawRotatedRect(x,y,width,height,degrees,color){
 
     ctx.fillStyle=color;
     ctx.fill();
-		
-	
+
+
         // restore the context to its untranslated/unrotated state
-    ctx.restore();
+        ctx.restore();
 
-}
+    }
 
 
 
-function game(){
-	var x,y,radians,steps, i;
-	
-    drawRotatedRect(0,10,80,50,0, 'white');	
-	ctx.font = "30px Arial";
-	ctx.fillText(cannon.angle, 10, 50);
-	
-	if (cannon.angle < 90)
-	{
-		cannon.angle = 90;
-	} else if (cannon.angle > 180)
-	{
-		cannon.angle = 180;
-	}
-	
+    function game(){
+    	var x,y,radians,steps, i;
+
+    	drawRotatedRect(0,10,80,50,0, 'white');	
+    	ctx.font = "30px Arial";
+    	ctx.fillText(cannon.angle, 10, 50);
+
+    	if (cannon.angle < 90)
+    	{
+    		cannon.angle = 90;
+    	} else if (cannon.angle > 180)
+    	{
+    		cannon.angle = 180;
+    	}
+
 	//console.log('Splat Travel '+cannon.splatTravel+' SplatOn '+cannon.splatOn);
 	x = cannon.splatX;
 	y = cannon.splatY;
 	radians = cannon.splatAngle;
 	i = cannon.splatTravel;		
 
-    if (cannon.splatOn == true)
+	if (cannon.splatOn == true)
 	{
 		drawArc(x-i*Math.cos(radians)*cannon.width,y-i*Math.sin(radians)*cannon.width+2,20,'dimgrey');						
 		cannon.splatTravel = cannon.splatTravel + 0.1;
@@ -139,12 +139,12 @@ function game(){
 		cannon.splatOn = false;
 	}
 	
-    drawArc(cannon.xPos+cannon.width/2, cannon.yPos+cannon.height/2, cannon.width/2+10, 'dimgray');
+	drawArc(cannon.xPos+cannon.width/2, cannon.yPos+cannon.height/2, cannon.width/2+10, 'dimgray');
 	
     // draw a rotated rect
     drawRotatedRect(cannon.xPos,cannon.yPos,cannon.width,cannon.height,cannon.angle, cannon.color);	
-	cannon.oldAngle = cannon.angle;
-	
+    cannon.oldAngle = cannon.angle;
+
 }
 // number of frames per second
 let framePerSecond = 50;
